ac69c902ec2e59dcee8f04680d6d8e64967046e5
LuisNie <nie.luyuan@gmail.com>
2017-01-17 09:33:28 -0800

    Update LuProgressBar.js

--- LuProgressBar.js
+++ LuProgressBar.js
@@ -3,7 +3,7 @@ $.fn.LuProgressBar = function (options) {
         currentTotal: 10,
         qualifiedShippingTotal: 100
     }, options);
-    this.append('<div id="myProgress" style="border-radius:5px;position: relative;height: 30px;width: 100%;background-color: gray;"><div id="myBar" style="border-radius:5px;position: absolute;height: 100%;background-color: #4CAF50;width:10%"> </div></div>');
+    this.html('<div id="myProgress" style="border-radius:5px;position: relative;height: 30px;width: 100%;background-color: gray;"><div id="myBar" style="border-radius:5px;position: absolute;height: 100%;background-color: #4CAF50;width:10%"> </div></div>');
     var move = function (currentValue, maxValue) {
         var percentage = parseInt((currentValue / maxValue) * 100);
         var current = 0;
